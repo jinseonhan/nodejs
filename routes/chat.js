3a77ba1c13f1915599773cc1a5807f798693c5cd
jshan <fantasi89@gmail.com>
2022-02-28 20:58:45 +0900

    msg insert

--- routes/chat.js
+++ routes/chat.js
@@ -16,16 +16,32 @@ router.post('/chatroom',function(req,res){
     var targetId = req.body.targetId;
     var userId = req.user.id;
     // 1. 기존 진행되고 있는 채팅방이 있었는지 확인 
-    db.collection('chatroom').find({member:{targetId,userId}}).toArray(function(error,result){
+    db.collection('chatroom').find({targetId:targetId,userId:userId}).toArray(function(error,result){
         // 2. 없다면 신규 생성해준다.
-       
+        console.log(result);
          // 3. 그 방으로 이동시킨다.
-         res.render('chatroom.ejs',{chat:result,userId:userId});
+         res.render('chatroom.ejs',{chat:result,userId:userId,targetId:targetId});
 
     });
 });
 
+// 채팅 내용 저장
+router.post('/memoSubmit',function(req,res){
+    var chat = req.body.chat;
+    var targetId = req.body.targetId;
+    var userId = req.user.id;
+    // TODO : 날짜 수정
+    var data = {chat:chat, targetId:targetId, userId:userId,date:new Date(),title:userId+"_"+targetId}
+    db.collection('chatroom').insertOne(data,function(error,result){
+        if(error){
+            res.send({message: 'fail', error:error});
 
+        }else{
+            res.send({message: 'success', error:error});
 
+        }
+
+    });
+});
   
 module.exports = router; // module.exports = 내보낼 변수명
