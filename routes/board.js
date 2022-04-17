25eac07ce1029423972a5a3be28429484afc2d58
jshan <fantasi89@gmail.com>
2022-04-17 17:40:53 +0900

    게시판 리스트(자신빼기, 좋아요시 이미지 변경, 취소/insert시 쿼리가 실행이 안됨

--- routes/board.js
+++ routes/board.js
@@ -20,13 +20,34 @@ router.use(loginCheck);
 // EJS사용(jstl과 비슷)
 // get 요청 : /list 로 접속하면 html에서 db의 데이터를 보여줌
 router.get('/list', function(req,res){
+    var userId=req.user.userId;
 
     // db애 저장된 post라는 collection안의 데이터 꺼내기
-    db.collection('post').find().toArray(function(error,result){
-      res.render('list.ejs',{posts: result});
+    // db.collection('post').find().toArray(function(error,result){
+    //   res.render('list.ejs',{posts: result});
+    // });
+    var params = [userId, userId];
+    // 내가쓴 글을 제외한 전체 리스트
+    var selectSql1 = 'select board_seq as boardSeq, title as title, view_count as viewCount, reg_id as reqId, favorite_count as favoriteCount, upt_dt as uptDt from nodejs.tb_user_board where reg_id <> ? order by	upt_dt desc';
+    
+    maria.query(selectSql1,params,function(err,result){
+        if(!err){
+            if(result.length>0){
+                // 나의 좋아요 리스트
+                var selectSql2 = 'select board_seq as boardSeq from nodejs.tb_board_favorite where favorite_name =?'; 
+                maria.query(selectSql2,params,function(err2,result2){
+                      if(!err2){
+                        res.render('list.ejs',{listResult:result,favResult:result2});
+                      }  
+                });
+                
+               // res.render('list.ejs');
+            }else{
+              // TODO : 에러시 에러 페이지로 이동
 
+            }
+          }
     });
- 
 });
 
 router.get('/write', function(req,res){
@@ -75,6 +96,11 @@ router.post('/boardAdd', (req,res)=>{
     
     // db.collection('counter').findOne({name: '게시물갯수'},function(error,result){
     //     let title = req.body.title;
+
+
+
+
+    
     //     let date = req.body.date;
     //     let register = req.user.id;
     
@@ -107,6 +133,75 @@ router.delete('/delete',function(req,res){
     });
 
 });
- 
+
+// 좋아요 클릭
+router.post('/fav', function(req,res){
+    var userId=req.user.userId;
+    var param=req.body.param; // 좋아요 취소/추가
+    var boardSeq=req.body.boardSeq; 
+    var params =[boardSeq,userId];
+    console.log("back");
+    var selectSql = 'select count(*) as cnt from nodejs.tb_board_favorite where board_seq =? and favorite_name =?';
+    maria.query(selectSql,params,function(err,result){
+        console.log("select");
+        if(!err){
+            if(result[0].cnt < 1){
+                // 기존 데이터가 없을 때는 insert 처리
+                var params2 = [boardSeq,userId,param,userId,userId];
+                console.log(params2);
+                var insertSql = 'insert into nodejs.tb_board_favorite(board_seq, favorite_name, fav, reg_dt, reg_id, upt_dt, upt_id) values(?,?,?,now(),?,now(),?)';
+                maria.query(insertSql,params2,function(err,rows,fields){
+                    console.log("insert");
+                    console.log(rows);
+                    console.log(fields);
+                    if(!err){
+                        
+                        res.writeHead(200,{'Content-Type':'text/html; charset=utf-8;'});
+                        if(rows.affectedRows>0){
+                            // 글 작성 성공!
+                            
+                            res.write("<script>alert('좋아요 완료!');location.href='/board/list';</script>");
+                            
+                            
+                           // res.render('list.ejs');
+                        }else{
+                          
+                          res.write("<script>alert('좋아요 실패!');</script>");
+                        }
+                      }
+                });
+            }else{
+                // 기존 데이터가 있을 때는 update 처리
+                var params3 = [boardSeq,userId,param,userId,boardSeq,userId];
+                var updateSql = 'update nodejs.tb_board_favorite set board_seq = ?, favorite_name = ?, fav = ? ,upt_dt=now(), upt_id = ? where board_seq=? and favorite_name=?';
+                maria.query(updateSql,params3,function(err,rows,fields){
+                    console.log("update");
+                    console.log(rows);
+                    console.log(fields);
+                    if(!err){
+                       
+                        res.writeHead(200,{'Content-Type':'text/html; charset=utf-8;'});
+                        if(rows.affectedRows>0){
+                            // update 성공!
+                            
+                            res.write("<script>alert('좋아요 완료!');location.href='/board/list';</script>");                            
+                            
+                        }else{
+                            // update 실패!
+                          res.write("<script>alert('좋아요 실패!');</script>");
+                        }
+                      }
+                });
+            }
+           
+        }
+    });
+
+   
+
+   
+    
+});
+
 
 module.exports = router; // module.exports = 내보낼 변수명
